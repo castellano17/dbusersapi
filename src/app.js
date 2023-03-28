1f4f9f798af73f56daac3482baa3a35f278de303
unknown <castellano17@gmail.com>
2023-03-27 21:59:13 -0600

    db config

---
+++ src/app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const userRouter = require("./users/users.router");
+const db = require("./utils/database");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+//? validar la conexión a la db
+db.authenticate()
+  .then(() => console.log("Database Authenticated!"))
+  .catch((err) => console.log(err));
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Server OK",
+    myMessage: process.env.MESSAGE,
+    myPort: process.env.PORT,
+  });
+});
+
+app.use("/", userRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server started at port ${PORT}`);
+});
